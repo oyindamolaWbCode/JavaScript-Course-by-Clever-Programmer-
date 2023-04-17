cf80f87a17b0db471d99bce384e540548d85baad
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2023-04-17 03:34:05 +0200

    object js

--- yourPlayground.js
+++ yourPlayground.js
@@ -1,4 +1,3 @@
-console.log('hello world')
 
 // Math Methods
 //Floor ()- Rounds down
@@ -22,3 +21,31 @@ console.log(groceries.indexOf('pear'))
  console.log(groceries.slice(0, 3))
 //  console.log(groceries.splice(3, 6))
  console.log(groceries.splice(1, 3))
+
+//  Objects
+
+const personal = {
+    name: 'Lampard',
+    shirt: "black"
+}
+
+console.log(personal.name)
+
+
+
+const introducerMe = (name, shirt) => {
+    const person = {
+    name:  name,
+    shirt: shirt,
+    assets: 5000000,
+    liabilty : 100000,
+    netWorth : function (){
+        return this.assets - this.liabilty
+    }
+}
+ const intro = `Hi, my name is ${person.name} and the color of my shirt is ${person.shirt} and my net worth is $ ${person.netWorth()} USD`
+
+ return intro
+}
+
+console.log(introducerMe('Oyindamola', 'flowery'))
